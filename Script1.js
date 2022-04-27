877cb57a4029f993b3964ed877c23ca350218bac
Wilson1610 <98386927+Wilson1610@users.noreply.github.com>
2022-04-27 21:46:06 +0100

    added script

--- Script1.js
+++ Script1.js
@@ -1,7 +1,12 @@
 function changePhoto() {
-    document.getElementById("pic1").src = "start.png"
+    document.getElementById("pic1").src = "start.png";
 }
 
 function returnPhoto(){
-    document.getElementById("pic1").src="image1.jpg"
+    document.getElementById("pic1").src = "image1.jpg";
+}
+
+function showvid() {
+    document.getElementById("myphoto").style.display = "none";
+    document.getElementById("vid1").style.display = "block";
 }
