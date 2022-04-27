bc12d052ad099e8fd48b21ed9a26c2ac682ebab6
Wilson1610 <98386927+Wilson1610@users.noreply.github.com>
2022-04-27 16:47:49 +0100

    addes pages

---
+++ Script1.js
@@ -0,0 +1,9 @@
+function changePhoto() {
+    document.getElementById("pic1").src = "start.png"
+    document.getElementById("pic1").style.height = "200px;"
+    document.getElementById("pic1").style.width = "500px;"
+}
+
+function returnPhoto(){
+    document.getElementById("pic1").src="image1.jpg"
+}
