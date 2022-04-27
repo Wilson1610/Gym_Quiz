0a3003c5f7ee0272a6f5455c948f7ca0b3278756
Wilson1610 <98386927+Wilson1610@users.noreply.github.com>
2022-04-27 19:06:34 +0100

    Fixes

--- Script1.js
+++ Script1.js
@@ -1,7 +1,5 @@
 function changePhoto() {
     document.getElementById("pic1").src = "start.png"
-    document.getElementById("pic1").style.height = "200px;"
-    document.getElementById("pic1").style.width = "500px;"
 }
 
 function returnPhoto(){
