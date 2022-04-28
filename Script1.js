dc4d488315f0a6bcbb14a1e2741c930170880d72
Wilson1610 <98386927+Wilson1610@users.noreply.github.com>
2022-04-28 17:23:47 +0100

    finished quiz2

--- Script1.js
+++ Script1.js
@@ -69,3 +69,68 @@ function result() {
         alert("If you have any questions or suggestions please go over to the contact me page and send me a question")
     }
 }
+
+function result2() {
+    
+    var quiz2score = 0;
+
+    if (document.getElementById('correct1').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct2').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct3').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct4').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct5').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct6').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct7').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct8').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct9').checked) {
+        quiz2score++
+    }
+
+    if (document.getElementById('correct10').checked) {
+        quiz2score++
+    }
+
+    if (quiz2score == 0) {
+        alert("You Got: " + quiz2score)
+        alert("Do you even lift bro?")
+    }
+
+    if (quiz2score > 0 && quiz2score <= 5) {
+        alert("You Got: " + quiz2score)
+        alert("Not too bad, not a gym lad yet though")
+    }
+
+    if (quiz2score > 5 && quiz2score <= 9) {
+        alert("You Got: " + quiz2score)
+        alert("Okay, Gym lad")
+    }
+
+    if (quiz2score == 10) {
+        alert("You Got: " + quiz2score)
+        alert("Daddy Cbum would be proud")
+    }
+}
